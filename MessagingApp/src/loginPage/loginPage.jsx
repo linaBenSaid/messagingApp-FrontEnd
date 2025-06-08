9f51c496287ff9230d76ccd564231ea6e3394c5a
neenolina <linabensaid700@gmail.com>
2025-06-08 20:03:20 +0100

    added authentication

--- MessagingApp/src/loginPage/loginPage.jsx
+++ MessagingApp/src/loginPage/loginPage.jsx
@@ -7,13 +7,53 @@ import { useNavigate } from 'react-router-dom';
 
 function loginPage(){
     const [userId, setUserId] = useState('');
-  const navigate = useNavigate();
+    const [password , setPassword] = useState('');
+  
+    const navigate = useNavigate();
 
-  const handleLogin = () => {
-    if (userId.trim()) {
-      localStorage.setItem('userId', userId);
-      navigate('/');
+  const handleLogin = async() => {
+    
+    if(userId.trim()){
+      try{
+        
+        const headers = {
+          'Content-Type' : 'application/json'
+        }
+
+        const body = {
+          username: userId,
+          password: password
+        }
+
+        const response = await fetch("http://localhost:3000/auth/login" , {
+          method: 'Post',
+          headers: headers,
+          body: JSON.stringify(body)
+        })
+
+        if(!response.ok) throw new Error('invalid username')
+        else{
+          const data = await response.json()
+
+          //stocker userId dans localstorage
+          // localStorage.setItem('userId', data.userId);
+
+          const { access_token } = data;
+          document.cookie = `token=${access_token}; path=/; secure`;
+          navigate('/');
+        }     
+    } catch (err) 
+    {
+      alert('Login failed');
     }
+    }
+    
+    
+    // old code 
+    // if (userId.trim()) {
+    //   localStorage.setItem('userId', userId);
+    //   navigate('/');
+    // }
   };
 
   return (
@@ -24,6 +64,12 @@ function loginPage(){
         placeholder="Enter your username"
         value={userId}
         onChange={(e) => setUserId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter your password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <button onClick={handleLogin} style={{ marginLeft: '1rem' }}>
         Login
